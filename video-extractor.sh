b4ecc2c7aaaf8375589d165025877b917c84ed77
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2021-03-19 09:24:54 +0000

    Tweaks

--- video-extractor.sh
+++ video-extractor.sh
@@ -3,4 +3,4 @@
 INPUT=$1
 OUTPUT=$2
 
-ffmpeg -ss 3 -i $FILENAME -vf "select=gt(scene\,0.4)" -frames:v 1 -vsync vfr $OUTPUT
+ffmpeg -ss 3 -i async:$INPUT -vf "select=gt(scene\,0.4)" -frames:v 1 -vsync vfr $OUTPUT
