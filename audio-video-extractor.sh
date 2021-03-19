3144b483c882d390d03f8fdc26cbf905735c66e4
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2021-03-19 10:51:09 +0000

    Extract cover art for video and audio.

--- audio-video-extractor.sh
+++ audio-video-extractor.sh
@@ -11,7 +11,7 @@
 # ./video-extractor.sh https://gateway.ipfs.io/ipfs/QmdKC2fXuf6soDkKwZswjmYwqNzTPjnhwCyBuXpBsUvZaV/Barry.S02E02.The.Power.of.No.1080p.AMZN.WEB-DL.DDP5.1.H.264-NTb.mkv thumb.jpg 640 480
 #
 # Formats: anything supported by FFMPEG
-# Example: MKV/MOV/MP4/JPEG/...(?)
+# Example: MKV/MOV/MP4/JPEG/MP3/OGG/FLAC/...
 #
 # SVG: 5 Your ffmpeg needs to be compiled with --enable-librsvg
 # Not supported: PDF, SVG, Office
@@ -23,4 +23,9 @@ HEIGHT=$4
 
 # Ref:
 # Scaling - https://superuser.com/questions/547296/resizing-videos-with-ffmpeg-avconv-to-fit-into-static-sized-player/1136305#1136305
-ffmpeg -ss 3 -i async:$INPUT -vf "select=gt(scene\,0.4), scale=$WIDTH:$HEIGHT:force_original_aspect_ratio=increase,crop=$WIDTH:$HEIGHT" -frames:v 1 -vsync vfr $OUTPUT
+
+# Try extractig cover art (allow only streams which are attached pictures, video thumbnails or cover arts).
+if ! ffmpeg -i async:$INPUT -map 0:v -map -0:V -vf "scale=$WIDTH:$HEIGHT:force_original_aspect_ratio=increase,crop=$WIDTH:$HEIGHT" -frames:v 1 $OUTPUT; then
+	# Extract first 40% scene change on a vframe (full frame), minimum 3s after start of video.
+	ffmpeg -ss 3 -i async:$INPUT -vf "select=gt(scene\,0.4), scale=$WIDTH:$HEIGHT:force_original_aspect_ratio=increase,crop=$WIDTH:$HEIGHT" -frames:v 1 -vsync vfr $OUTPUT
+fi
