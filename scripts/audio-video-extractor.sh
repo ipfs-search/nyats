1bfca9bba8d3a2398d3226434976baf72ed36e5c
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2021-07-31 11:54:50 +0200

    Streaming for ffmpeg too.

--- scripts/audio-video-extractor.sh
+++ scripts/audio-video-extractor.sh
@@ -5,10 +5,10 @@
 # after the first 3s, in order to yield more meaningful thumbnails.
 #
 # Usage:
-# video-extractor.sh $INPUT $OUTPUT $WITDH $HEIGHT
+# audio-video-extractor.sh $INPUT $WITDH $HEIGHT
 #
 # Example:
-# ./video-extractor.sh https://gateway.ipfs.io/ipfs/QmdKC2fXuf6soDkKwZswjmYwqNzTPjnhwCyBuXpBsUvZaV/Barry.S02E02.The.Power.of.No.1080p.AMZN.WEB-DL.DDP5.1.H.264-NTb.mkv thumb.jpg 640 480
+# ./audio-video-extractor.sh https://gateway.ipfs.io/ipfs/QmdKC2fXuf6soDkKwZswjmYwqNzTPjnhwCyBuXpBsUvZaV/Barry.S02E02.The.Power.of.No.1080p.AMZN.WEB-DL.DDP5.1.H.264-NTb.mkv 640 480 > thumb.jpg
 #
 # Formats: anything supported by FFMPEG
 # Example: MKV/MOV/MP4/JPEG/MP3/OGG/FLAC/...
@@ -19,15 +19,14 @@
 # TODO: Check for 'cover.jpg' and/or cover.png in folder (if present) and use that over extracting.
 
 INPUT=$1
-OUTPUT=$2
-WIDTH=$3
-HEIGHT=$4
+WIDTH=$2
+HEIGHT=$3
 
 # Ref:
 # Scaling - https://superuser.com/questions/547296/resizing-videos-with-ffmpeg-avconv-to-fit-into-static-sized-player/1136305#1136305
 
 # Try extractig cover art (allow only streams which are attached pictures, video thumbnails or cover arts).
-if ! ffmpeg -i async:$INPUT -map 0:v -map -0:V -vf "scale=$WIDTH:$HEIGHT:force_original_aspect_ratio=increase,crop=$WIDTH:$HEIGHT" -frames:v 1 $OUTPUT; then
+if ! ffmpeg -i async:$INPUT -map 0:v -map -0:V -vf "scale=$WIDTH:$HEIGHT:force_original_aspect_ratio=increase,crop=$WIDTH:$HEIGHT" -frames:v 1 -f singlejpeg -; then
 	# Extract first 40% scene change on a vframe (full frame), minimum 3s after start of video.
-	ffmpeg -ss 3 -i async:$INPUT -vf "select=gt(scene\,0.4), scale=$WIDTH:$HEIGHT:force_original_aspect_ratio=increase,crop=$WIDTH:$HEIGHT" -frames:v 1 -vsync vfr $OUTPUT
+	ffmpeg -ss 3 -i async:$INPUT -vf "select=gt(scene\,0.4), scale=$WIDTH:$HEIGHT:force_original_aspect_ratio=increase,crop=$WIDTH:$HEIGHT" -frames:v 1 -vsync vfr -f singlejpeg  -
 fi
