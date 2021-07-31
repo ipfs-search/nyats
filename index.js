3c21c80fc26a39428352bfa54f40914200e97d41
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2021-07-31 18:14:37 +0200

    Something is doing something.

--- index.js
+++ index.js
@@ -1,37 +1,47 @@
 const express = require('express');
+const ipfsClient = require('ipfs-http-client');
+const createThumbnailer = require('./thumbnailer');
 
 const app = express();
 const port = 9614;
 
-const thumbnailer = require('./thumbnailer');
+async function main() {
+  const ipfs = await ipfsClient.create();
 
-// Custom error responses
-function error(res, code, err) {
-  console.error(`${code}: ${err}`);
-  console.trace(err);
+  const version = await ipfs.version()
+  console.log('IPFS Version:', version.version)
 
-  res.json({ error: `${err}` }).status(code).end();
+  const thumbnailer = createThumbnailer(ipfs);
+
+  app.get('/thumbnail/:protocol/:cid/:width/:height', async (req, res, next) => {
+    // TODO: Validation
+    // https://express-validator.github.io/docs/
+    const { protocol, cid, width, height } = req.params;
+    const url = await thumbnailer(protocol, cid, width, height);
+    res.redirect(url);
+  });
+
+  function error(res, code, err) {
+    console.error(`${code}: ${err}`);
+    console.trace(err);
+
+    res.json({ error: `${err}` }).status(code).end();
+  }
+
+  app.use(function (err, req, res, next) {
+    if (process.env.NODE_ENV === 'production') {
+      // Don't leak details in production
+      error(res, 500, 'Internal Server Error');
+    }
+
+    if (res.headersSent) {
+      return next(err);
+    }
+
+    error(res, 500, err);
+  });
+
+  app.listen(port, () => console.log(`nyats server listening on port ${port}!`));
 }
 
-app.get('/thumbnail/:protocol/:cid/:width/:height', (req, res, next) => {
-  // TODO: Validation
-  // https://express-validator.github.io/docs/
-  const { protocol, cid, width, height } = req.params;
-  const url = thumbnailer(protocol, cid, width, height);
-  res.redirect(url);
-});
-
-app.use(function (err, req, res, next) {
-  if (process.env.NODE_ENV === 'production') {
-    // Don't leak details in production
-    error(res, 500, 'Internal Server Error');
-  }
-
-  if (res.headersSent) {
-    return next(err);
-  }
-
-  error(res, 500, err);
-});
-
-app.listen(port, () => console.log(`nyats server listening on port ${port}!`));
+main()
--- thumbnailer.js
+++ thumbnailer.js
@@ -1,5 +1,36 @@
-module.exports = function(protocol, cid, width, height) {
-  console.log(protocol);
+const assert = require('assert').strict;
+const sharp = require('sharp');
+const stream = require('stream');
 
-  return 'https://gateway.ipfs.io/ipfs/bafkreieautudtelplpd7322hl2dxcnkza7x7s4iiy7cyecfohf672l7z2y';
+module.exports = function(ipfs) {
+  async function _getThumbnail(source) {
+    // Note: source should be of type AsyncIterable<Uint8Array> but is '[object AsyncGenerator]' of type object
+    // which sharp refuses.
+    let shittyFile = new Uint8Array()
+    for await (const chunk of source) {
+      shittyFile += chunk
+      console.log(chunk)
+    }
+
+    return sharp(shittyFile).resize(300)
+      .on('info', function(info) {
+        console.log('Image height is ' + info.height);
+      });
+  }
+  function _addIPFS(f) {
+
+  }
+  function _ipfsURL(cid) {
+    return `https://gateway.ipfs.io/ipfs/${cid}`;
+  }
+
+  return async function(protocol, cid, width, height) {
+    assert.equal(protocol, 'ipfs');
+
+    const input = ipfs.cat(`/${protocol}/${cid}`);
+    const thumbnail = await _getThumbnail(input);
+    const thumbnailCid = _addIPFS(thumbnail);
+
+    return _ipfsURL(cid);
+  }
 };
