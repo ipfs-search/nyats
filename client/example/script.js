ad762247abd57b38db78c638f0a017e9e43c7477
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2021-08-04 10:03:50 +0200

    Client WIP.

---
+++ client/example/script.js
@@ -0,0 +1,8 @@
+$(() => {
+  const nyats = nyats_client.create({
+    nyats_server: 'http://localhost:9614',
+    ipfs_gateway: 'https://gateway.ipfs.io'
+  })
+  const container = $('#container');
+
+});
