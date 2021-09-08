45e009e41944e3171b57e9843f0ab44920ccd6e7
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2021-09-08 14:54:05 +0100

    Video thumbnail WIP.

--- server/video_thumbnailer.js
+++ server/video_thumbnailer.js
@@ -1,46 +1,117 @@
-// const assert = require('assert').strict;
-const { spawn } = require('child_process');
+const util = require('util');
+const childProcess = require('child_process');
+const spawn = util.promisify(childProcess.spawn);
 const debug = require('debug')('nyats:video_thumbnailer');
 const pathToFfmpeg = require('ffmpeg-static');
 
-module.exports = () => {
-  // # Try extractig cover art (allow only streams which are attached pictures, video thumbnails or cover arts).
-  // if ! ffmpeg -i async:$INPUT -map 0:v -map -0:V -vf "scale=$WIDTH:$HEIGHT:force_original_aspect_ratio=increase,crop=$WIDTH:$HEIGHT" -frames:v 1 -f singlejpeg -; then
-  // Coverart extractor (attempt 1)
-  // const transcoder = new prism.FFmpeg({
-  //   args: [
-  //     '-analyzeduration', '0',
-  //     '-loglevel', '0',
-  //     '-f', 's16le',
-  //     '-ar', '48000',
-  //     '-ac', '2',
-  //   ]
-  // });
-  return {
-    makeThumbnail(url, width, height) {
-      //Extract first 40% scene change on a vframe (full frame), minimum 3s after start of video.
+async function ffmpegExtractor(params) {
+  const commonParams = [
+    '-f', 'singlejpeg',
+    '-',
+    '-loglevel', 'info',
+    '-hide_banner',
+  ];
 
-      const ffmpeg = spawn(
-        pathToFfmpeg,
-        [
-          // '-f', 'matroska', // ffmpeg's type detection seems to work 'fine'
-          '-ss', '1', // skip first second
-          '-i', `async:${url}`,
-          '-vf', `scale=${width}:${height}:force_original_aspect_ratio=increase,crop=${width}:${height}`,
-          '-frames:v', '1',
-          '-vsync', 'vfr',
-          '-f', 'singlejpeg',
-          '-',
-          '-loglevel', 'info',
-          '-hide_banner',
-        ]
-      )
-
-      ffmpeg.stderr.on('data', (data) => {
-        debug(data.toString('utf8'));
-      });
-
-      return ffmpeg.stdout;
+  // WARN/TODO:
+  // Never pass unsanitized user input to this function. Any input containing shell metacharacters
+  // may be used to trigger arbitrary command execution!!!
+  const ffmpeg = spawn(
+    pathToFfmpeg,
+    params.concat(commonParams),
+    {
+      encoding: 'buffer',
+      windowsHide: true,
+      timeout: 30000, // 30s timeout by default
     }
+  );
+
+  // ffmpeg.child.stderr.on('data', (data) => {
+  //   debug(data.toString('utf8'));
+  // });
+
+  return ffmpeg;
+}
+
+function scaleFilter(width, height) {
+  return `scale=${width}:${height}:force_original_aspect_ratio=increase,crop=${width}:${height}`;
+}
+
+async function extractCoverArt(url, width, height) {
+  // TODO: Use cover art archive as secondary source.
+  // https://coverartarchive.org/release/79de4fa8-102c-402f-8040-3f1aa6d0c1b4/front
+  debug('Try extractig cover art for streams with attached pictures, thumbnails or cover art.');
+
+  return ffmpegExtractor(
+    [
+      '-i', `async:${url}`,
+      '-map 0:v',
+      '-map -0:V',
+      '-vf', scaleFilter(width, height),
+      '-frames:v 1',
+    ]
+  );
+}
+
+async function extractKeyFrame(url, width, height) {
+  debug('Extract first 40% scene change on a vframe (full frame), minimum 3s after start of video.');
+
+  const vFilter = `select=gt(scene\,0.4), ${scaleFilter(width, height)}`;
+
+  return ffmpegExtractor(
+    [
+      '-ss 3', // Skip first 3s
+      '-i', `async:${url}`,
+      '-vf', vFilter,
+      '-frames:v', '1',
+      '-vsync', 'vfr',
+    ]
+  );
+}
+
+async function extractFirstFrame(url, width, height) {
+  debug('Extracting first frame');
+  return ffmpegExtractor(
+    [
+      // '-f', 'matroska', // ffmpeg's type detection seems to work 'fine'
+      '-i', `async:${url}`,
+      '-vf', scaleFilter(width, height),
+      '-frames:v', '1',
+      '-vsync', 'vfr',
+    ]
+  );
+}
+
+function init() {
+
+}
+
+module.exports = () => {
+  init();
+
+  return {
+    async makeThumbnail(url, width, height) {
+      try {
+        const { stdout } = await extractCoverArt(url, width, height);
+        return stdout;
+      } catch (e) {
+        debug(e);
+      }
+
+      try {
+        const { stdout } = await extractKeyFrame(url, width, height);
+        return stdout;
+      } catch (e) {
+        debug(e);
+      }
+
+      try {
+        const { stdout } = await extractFirstFrame(url, width, height);
+        return stdout;
+      } catch (e) {
+        debug(e);
+      }
+
+      throw new Error('All thumbnail methods failed.');
+    },
   };
 };
