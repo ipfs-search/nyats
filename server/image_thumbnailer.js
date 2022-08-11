3947e66543acd1346fdb015abeae8f11485a9538
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2022-08-11 12:14:17 +0100

    White background for images with transparency.

--- server/image_thumbnailer.js
+++ server/image_thumbnailer.js
@@ -1,11 +1,15 @@
 const sharp = require('sharp');
 
+const white = {r:255, g:255, b:255}; // Default: white background
+
 module.exports = () => {
   return {
     makeThumbnail(stream, width, height) {
       const transformer = sharp()
+        .flatten({ background: white })
         .resize(width, height, {
           position: sharp.strategy.attention,
+          background: white
         })
         .toFormat('jpeg', {
           mozjpeg: true,
