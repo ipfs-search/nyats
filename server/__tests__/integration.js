f957f2678823fbd7a6f49758f333a31ddd81437b
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2022-08-19 15:58:38 +0100

    Mock WIP

--- server/__tests__/integration.js
+++ server/__tests__/integration.js
@@ -1,3 +1,4 @@
+import { jest } from "@jest/globals";
 import jestOpenAPI from "jest-openapi";
 import request from "supertest";
 import path from "path";
@@ -7,9 +8,26 @@ import app from "../app.js";
 // Not sure why .default here is necessary.
 jestOpenAPI.default(path.resolve("openapi.yml"));
 
+let statMock = jest.fn(() => Promise.reject());
+let versionMock = jest.fn(() => Promise.resolve({ version: "0.14.0" }));
+jest.mock("ipfs-http-client", {
+  create: function () {
+    return {
+      version: versionMock,
+      files: {
+        stat: statMock,
+      },
+    };
+  },
+});
+
 describe("GET IPFS thumbnail", () => {
+  const rapp = request(app);
+
   it("Should satisfy OpenAPI spec", async () => {
-    const res = await request(app).get(
+    // statMock.mockReturnValue(Promise.reject());
+
+    const res = await rapp.get(
       "/thumbnail/ipfs/QmcRD4wkPPi6dig81r5sLj9Zm1gDCL4zgpEj9CfuRrGbzF/200/200"
     );
 
