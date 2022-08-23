829774fd7fe181b27113b87ea1710c1461bd8bef
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2022-08-23 20:13:25 +0100

    Basic integration test running.

--- server/test/integration.js
+++ server/test/integration.js
@@ -1,67 +1,81 @@
-/////
-// Setup testing infra
-////
+"strict";
 import { use, expect } from "chai";
+import sinon from "sinon";
 import request from "supertest";
 import path from "path";
+import fs from "fs";
+import esmock from "esmock";
 
-// Testdouble
-import * as td from "testdouble";
-import tdChai from "testdouble-chai";
-use(tdChai(td));
+// Use Sinon with Chai
+import sinonChai from "sinon-chai";
+use(sinonChai);
 
 // OpenAPI matcher
 import { chaiPlugin as matchApiSchema } from "api-contract-validator";
 const apiDefinitionsPath = path.resolve("openapi.yml");
 use(matchApiSchema({ apiDefinitionsPath }));
 
-import makeApp from "../app.js";
+async function grapefruitAsyncIterator() {
+  const filepath = path.resolve("server/test/grapefruit.jpg");
+  const readStream = fs.createReadStream(filepath, {
+    highWaterMark: 1024,
+  });
 
-// // Not sure why .default here is necessary.
-// jestOpenAPI.default();
+  return readStream;
+}
 
-// // let statMock = jest.fn(() => Promise.reject());
-// let versionMock = jest.fn(() => Promise.resolve({ version: "0.99.0" }));
-// // jest.mock("ipfs-http-client", () => {
-// //   create: function () {
-// //     return {
-// //       version: versionMock,
-// //       files: {
-// //         stat: statMock,
-// //       },
-// //     };
-// //   },
-// // });
+describe("Thumbnail generator", () => {
+  describe("Newly generated thumbnail", () => {
+    let response;
 
-// // const createMock = jest.fn();
-// jest.mock("ipfs-http-client");
+    const cid = "QmcRD4wkPPi6dig81r5sLj9Zm1gDCL4zgpEj9CfuRrGbzF";
+    const width = 200;
+    const height = 200;
+    const protocol = "ipfs";
 
-// create.mockImplementation(() => {
-//   console.log("go to hell");
-// });
+    const statStub = sinon.stub();
+    statStub.throws("not found", "file does not exist");
 
-let ipfsMock;
-describe("GET IPFS thumbnail", () => {
-  beforeEach(() => {
-    ipfsMock = await td.replace("ipfs-http-client", () => {
-      version: () => Promise.resolve({ version: "0.99.0" })
+    const catStub = sinon.stub().resolves(grapefruitAsyncIterator());
+    const addStub = sinon.stub().resolves({
+      cid: "thumbnailCid",
+      size: 34,
     });
-  });
-  afterEach(() => {
-    td.reset();
-  });
+    const cpStub = sinon.stub().resolves();
+    const flushStub = sinon.stub().resolves();
 
-  it("Should satisfy OpenAPI spec", async () => {
-    // const versionMock = jest.fn(() => Promise.resolve({ version: "0.99.0" }));
-    // create.mockReturnValue({
-    //   version: versionMock,
+    // statStub.withArgs("/${cid}-${width}-${height}.webp").throws(
+    //   new Error({
+    //     message: "file does not exist",
+    //   })
+    // );
+    // statStub.withArgs("/").returns({
+    //   cid: "rootCid",
     // });
-    // statMock.mockReturnValue(Promise.reject());
-    const app = request(await makeApp());
-    const response = await app.get(
-      "/thumbnail/ipfs/QmcRD4wkPPi6dig81r5sLj9Zm1gDCL4zgpEj9CfuRrGbzF/200/200"
-    );
 
-    expect(response).to.have.status(200).and.to.matchApiSchema();
+    const ipfsMock = {
+      version: sinon.stub().resolves({ version: "0.99.0" }),
+      add: addStub,
+      cat: catStub,
+      files: {
+        stat: statStub,
+        cp: cpStub,
+        flush: flushStub,
+      },
+    };
+
+    beforeEach(async function () {
+      const makeApp = await esmock("../app.js", {
+        "ipfs-http-client": {
+          create: () => ipfsMock,
+        },
+      });
+      const app = request(await makeApp());
+      response = await app.get(`/thumbnail/${protocol}/${cid}/${width}/${height}`);
+    });
+
+    it("Should satisfy OpenAPI spec", async function () {
+      expect(response).to.have.status(301).and.to.matchApiSchema();
+    });
   });
 });
