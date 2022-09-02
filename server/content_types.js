4771cf795c929082b163ff77004fc4ca4d8b65bf
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2022-09-02 21:25:26 +0100

    Prettier.

--- server/content_types.js
+++ server/content_types.js
@@ -1,41 +1,32 @@
 const types = {
   text: [
     // eBook types
-    'application/x-mobipocket-ebook',
-    'application/epub+zip',
-    'application/vnd.amazon.ebook',
+    "application/x-mobipocket-ebook",
+    "application/epub+zip",
+    "application/vnd.amazon.ebook",
     // Scanned documents
-    'image/vnd.djvu',
-    'application/pdf',
+    "image/vnd.djvu",
+    "application/pdf",
     // HTML/plain text
-    'text/html',
-    'text/plain',
+    "text/html",
+    "text/plain",
     // Text editors
-    'application/postscript',
-    'application/rtf',
+    "application/postscript",
+    "application/rtf",
     // Open Office et al.
-    'application/vnd.oasis.opendocument.text',
-    'application/vnd.sun.xml.writer',
-    'application/vnd.stardivision.writer',
-    'application/x-starwriter',
+    "application/vnd.oasis.opendocument.text",
+    "application/vnd.sun.xml.writer",
+    "application/vnd.stardivision.writer",
+    "application/x-starwriter",
     // MS Word
-    'application/msword',
-    'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    "application/msword",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
     // Misc
-    'application/x-abiword'
+    "application/x-abiword",
   ],
-  audio: [
-    'audio/*',
-    'application/ogg'
-  ],
-  video: [
-    'video/*',
-    'application/mp4',
-    'application/x-matroska'
-  ],
-  image: [
-    'image/*',
-  ],
+  audio: ["audio/*", "application/ogg"],
+  video: ["video/*", "application/mp4", "application/x-matroska"],
+  image: ["image/*"],
 };
 
 export default types;
--- server/image_thumbnailer.js
+++ server/image_thumbnailer.js
@@ -1,24 +1,24 @@
-import debuggerFactory from 'debug';
-import sharp from 'sharp';
+import debuggerFactory from "debug";
+import sharp from "sharp";
 
-const debug = debuggerFactory('nyats:image_thumbnailer');
+const debug = debuggerFactory("nyats:image_thumbnailer");
 
 function isLossless(metadata) {
   switch (metadata.format) {
-    case 'jpeg':
+    case "jpeg":
       return false;
-    case 'png':
-    case 'gif':
-    case 'svg':
+    case "png":
+    case "gif":
+    case "svg":
       return true;
-    case 'webp':
+    case "webp":
       // Consistent with the VP8 bitstream, lossy WebP works exclusively with
       // an 8-bit Y'CbCr 4:2:0 (often called YUV420) image format. Please
       // refer to Section 2, "Format Overview" of RFC 6386, VP8 Data Format
       // and Decoding Guide for more detail. Lossless WebP works exclusively
       // with the RGBA format. See the WebP Lossless Bitstream specification.
       // https://developers.google.com/speed/webp/faq
-      return metadata.chromaSubsampling !== '4:2:0';
+      return metadata.chromaSubsampling !== "4:2:0";
     default:
       throw new Error(`Unexpected format: ${metadata.format}`);
   }
@@ -27,12 +27,12 @@ function isLossless(metadata) {
 async function makeThumbnail(stream, width, height) {
   const image = sharp({
     animated: true,
-    failOn: 'error',
+    failOn: "error",
   });
   stream.pipe(image);
 
   const metadata = await image.clone().metadata();
-  debug('Got metadata:', metadata);
+  debug("Got metadata:", metadata);
 
   const isAnimated = metadata.pages > 1;
 
