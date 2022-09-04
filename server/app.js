f59d2a3e2764a25af5e8509497634ebf6a513d64
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2022-09-04 18:50:38 +0100

    WIP.

--- server/app.js
+++ server/app.js
@@ -8,6 +8,15 @@ import { ipfsGateway } from "./conf.js";
 import makeDebugger from "debug";
 const debug = makeDebugger("nyats:server");
 
+function getGatewayURL(req, ipfsPath) {
+  // We can use the referer to derive whether we've been requested from a gateway
+  // and then use this to generate URL's.
+  const referer = req.headers["referer"];
+  debug(referer);
+
+  return urlJoin(ipfsGateway, ipfsPath);
+}
+
 export default (thumbnailer) => {
   const app = express();
 
@@ -26,7 +35,7 @@ export default (thumbnailer) => {
       assert(ipfsPath);
 
       res.setHeader("x-ipfs-path", ipfsPath);
-      const redirectURL = urlJoin(ipfsGateway, ipfsPath);
+      const redirectURL = getGatewayURL(req, ipfsPath);
 
       debug(`Redirecting to ${redirectURL}`);
       res.redirect(301, redirectURL);
