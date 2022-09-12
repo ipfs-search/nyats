e07548e05ee4ec3dd339f0853c4eb8008329092b
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2022-09-12 09:28:18 +0100

    Vite build for Vue component (WIP).

---
+++ packages/vuetify-img/src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import VImgCid from "./components/vuetify-img-cid.vue";
+</script>
+
+<template>
+  <VImgCid cid="QmVdwDzK2MuhHZaTdBHrCcofqQSET5pKK1j578rSkkY7rB" />
+</template>
