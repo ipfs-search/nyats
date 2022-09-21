f3fcb38d87213ca2ea87e295b3af68c0d3f4fa77
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2022-09-21 11:39:13 +0100

    More cleanup.

--- packages/server/src/ipns_publisher.ts
+++ packages/server/src/ipns_publisher.ts
@@ -1,13 +1,17 @@
 import debuggerFactory from "debug";
+import type { IPFS } from "ipfs-core-types";
 
 const debug = debuggerFactory("nyats:ipns_publisher");
 
-export default async function startIPNSPublisher(ipfs, updateInterval) {
+export default async function startIPNSPublisher(
+  ipfs: IPFS,
+  updateInterval: number
+): Promise<NodeJS.Timer> {
   // Publish to IPNS every minute, but only if the root was changed
   const root = await ipfs.files.stat("/", { hash: true });
   let rootCidStr = root.cid.toString();
 
-  const timeout = setInterval(async () => {
+  async function publishRoot() {
     // Publish to IPNS - normally we only want to do this every few minutes or so,
     // this is a client-side cache. Don't wait though!
     const newroot = await ipfs.files.stat("/", { hash: true });
@@ -22,7 +26,9 @@ export default async function startIPNSPublisher(ipfs, updateInterval) {
         console.log("Error publishing to IPNS:", e);
       }
     }
-  }, updateInterval);
+  }
+
+  const timeout = setInterval(publishRoot, updateInterval);
 
   console.log("Started IPNS publisher with interval %sms", updateInterval);
 
