cc237d8d6491d247983ad52337720328129a9141
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2022-09-21 11:05:20 +0100

    More cleanup.

--- packages/server/src/image_thumbnailer.ts
+++ packages/server/src/image_thumbnailer.ts
@@ -12,7 +12,7 @@ export class ValidationError extends Error {
   }
 }
 
-function isLossless(metadata): boolean {
+function isLossless(metadata: sharp.Metadata): boolean {
   switch (metadata.format) {
     case "jpeg":
     case "heif":
@@ -35,34 +35,38 @@ function isLossless(metadata): boolean {
   }
 }
 
-async function makeThumbnail(stream, width, height) {
-  const image = sharp({
-    animated: animateThumbnails,
-    failOn: "error",
-  });
-  stream.pipe(image);
-
-  const metadata = await image.clone().metadata();
-  debug("Got metadata:", metadata);
-
+function validateMetadata(metadata: sharp.Metadata) {
   // Validate input data, prevent PNG bombs and the likes.
   if (metadata.width > nyatsMaxInputWidth || metadata.height > nyatsMaxInputHeight) {
     throw new ValidationError(
       `Image dimensions (${metadata.width}x${metadata.height}) exceed maximum input dimensions.`
     );
   }
+}
 
+function getWebpOptions(metadata: sharp.Metadata, isAnimated: boolean): sharp.WebpOptions {
   const webpOptions: sharp.WebpOptions = {
     lossless: isLossless(metadata),
   };
-  const isAnimated = animateThumbnails && metadata.pages > 1;
 
   if (isAnimated) {
     webpOptions.loop = metadata.loop;
     webpOptions.delay = metadata.delay;
   }
 
-  const transformer = image
+  return webpOptions;
+}
+
+function getTransformer(
+  image: sharp.Sharp,
+  metadata: sharp.Metadata,
+  width: number,
+  height: number
+) {
+  const isAnimated = animateThumbnails && metadata.pages > 1;
+  const webpOptions = getWebpOptions(metadata, isAnimated);
+
+  return image
     .resize(width, height, {
       // Attention doesn't work for animations
       position: isAnimated ? sharp.gravity.center : sharp.strategy.attention,
@@ -71,8 +75,30 @@ async function makeThumbnail(stream, width, height) {
       seconds: 120,
     })
     .webp(webpOptions);
+}
 
-  return transformer;
+async function getMetadata(image: sharp.Sharp): Promise<sharp.Metadata> {
+  return image.clone().metadata();
+}
+
+function getImage(stream: NodeJS.ReadableStream): sharp.Sharp {
+  const image = sharp({
+    animated: animateThumbnails,
+    failOn: "error",
+  });
+  stream.pipe(image);
+  return image;
+}
+
+async function makeThumbnail(stream: NodeJS.ReadableStream, width: number, height: number) {
+  const image = getImage(stream);
+
+  const metadata = await getMetadata(image);
+  debug("Got metadata:", metadata);
+
+  validateMetadata(metadata);
+
+  return getTransformer(image, metadata, width, height);
 }
 
 export default function makeThumbnailer() {
